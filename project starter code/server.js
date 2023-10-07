7846bebc371436677e2d6955e129991b2a2684b4
David Stiennon <david.stiennon@clearcapital.com>
2023-10-07 10:11:53 -0500

    download images before passing them to Jimp

--- project starter code/server.js
+++ project starter code/server.js
@@ -1,23 +1,41 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
+import {filterImageFromURL, deleteLocalFiles, downloadToLocal} from './util/util.js';
 
   // Init the Express application
   const app = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
+  const port = process.env.PORT || 8080;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+// GET /filteredimage?image_url={{URL}}
+// endpoint to filter an image from a public url.
+// IT SHOULD
+//    1
+//    1. validate the image_url query
+//    2. call filterImageFromURL(image_url) to filter the image
+//    3. send the resulting file in the response
+//    4. deletes any files on the server on finish of the response
+// QUERY PARAMATERS
+//    image_url: URL of a publicly accessible image
+// RETURNS
+//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+/**************************************************************************** */
+
+//! END @TODO1
   app.get('/filteredimage', async (req, res) => {
     if(!req.query.image_url) {
       res.status(400).send('must include image_url query parameter')
     }
     const image_url = req.query.image_url
     try {
-      const finalUrl = await filterImageFromURL(image_url)
+      const bufferedImagePath = await downloadToLocal(image_url)
+      const finalUrl = await filterImageFromURL(bufferedImagePath)
       if (!finalUrl) {
         throw new Error('empty url')
       }
@@ -28,24 +46,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
       res.status(422).send(`Unable to process image at ${image_url}`)
     }
   })
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-    /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async (req, res) => {
--- project starter code/util/util.js
+++ project starter code/util/util.js
@@ -1,8 +1,41 @@
 import fs from "fs";
 import Jimp from "jimp";
+import * as https from "https";
 
 
 export const tempDir = `${process.cwd()}/tmp`
+
+export async function downloadToLocal(image_url) {
+    return new Promise((resolve, reject) => {
+        const parts = image_url.split('.')
+        const extension = parts[parts.length - 1]
+        const outpath =
+            `${tempDir}/buffered.${Math.floor(Math.random() * 2000)}.${extension}`;
+
+        const file = fs.createWriteStream(outpath);
+        https.get(image_url, (response) => {
+            if (response.statusCode !== 200) {
+                const message = `Failed to get '${image_url}' (${response.statusCode})`;
+                reject(message)
+                return;
+            }
+
+            // Pipe the response stream (downloaded image) into the file stream (local file)
+            response.pipe(file);
+
+            file.on('finish', () => {
+                // Close the file stream
+                file.close();
+                console.log('Image downloaded and saved!');
+                resolve(outpath)
+            });
+        }).on('error', (err) => {
+            // Handle errors like DNS resolution, TCP level errors, etc.
+            const error = 'Error downloading image:' + err.message;
+            reject(error)
+        });
+    })
+}
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -38,7 +71,7 @@ export async function deleteLocalFiles({except_for}) {
         const full_file = `${tempDir}/${file}`
         try {
             // clean up directory, but don't delete the file we just created
-            if(full_file !== except_for) {
+            if (full_file !== except_for) {
                 fs.unlinkSync(full_file);
             }
         } catch (e) {
